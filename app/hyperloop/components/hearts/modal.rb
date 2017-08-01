abc3c7ae8c83b7ea1f853fa19bdd7bbe4aee5d8d
Barrie Hadfield <barrie@workshare.com>
2017-08-01 17:46:16 +0100

    with actions

--- app/hyperloop/components/hearts/modal.rb
+++ app/hyperloop/components/hearts/modal.rb
@@ -5,12 +5,27 @@ class HeartModal < Hyperloop::Component
 
   render(DIV) do
     Sem.Modal(trigger: params.trigger.to_n) {
-      Sem.ModalHeader { "Heart Card" }
+      Sem.ModalHeader {
+        params.heart.name.nil? || params.heart.name.length == 0 ? "New Heart Card" : params.heart.name
+      }
       Sem.ModalContent {
-        # Sem.ModalDescription {
-          P {"modal body"}
-        # }
+        content
+      }
+      Sem.ModalActions {
+        actions
       }
     }
   end
+
+  def content
+    Sem.Input(placeholder: "Heart Card name", fluid: true, defaultValue: params.heart.name).on(:change) do |e|
+      params.heart.name = e.target.value
+    end
+    H5 { "Please choose one or more categories from the HEART framework you would like to track" }
+  end
+
+  def actions
+    Sem.Button { "Close" }
+  end
+
 end
