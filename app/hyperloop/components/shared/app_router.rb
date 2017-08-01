e60a785afd8da5530b2a0d03f0faf45202f4e049
Barrie Hadfield <barrie@workshare.com>
2017-08-01 14:06:08 +0100

    setup from base

--- app/hyperloop/components/shared/app_router.rb
+++ app/hyperloop/components/shared/app_router.rb
@@ -5,9 +5,6 @@ class AppRouter < Hyperloop::Router
     DIV {
       Switch do
         Route('/', exact: true, mounts: Home)
-        Route('/exercises', exact: true, mounts: Exercises)
-        Route('/muscle-groups', exact: true, mounts: MuscleGroups)
-        Route('/rule-sets', exact: true, mounts: RuleSets)
         Route('/members', exact: true, mounts: Members)
         # Route('/team/:id') { |m, l, h| Teams::Show(match: m, location: l, history: h) }
       end
--- app/hyperloop/components/shared/main_app_bar.rb
+++ app/hyperloop/components/shared/main_app_bar.rb
@@ -8,9 +8,6 @@ class MainAppBar < Hyperloop::Router::Component
     Sem.Menu(fixed: :top, borderless: true, color: :red, inverted: true, size: :huge) {
       Sem.Container {
         Sem.MenuItem { home }
-        Sem.MenuItem { exercises }
-        Sem.MenuItem { muscle_groups }
-        Sem.MenuItem { rule_set }
         Sem.MenuItem { members }
       }
     }
@@ -18,18 +15,6 @@ class MainAppBar < Hyperloop::Router::Component
 
   def home
     NavLink("/", class: "navlink", active_class: 'active') { "HOME" }
-  end
-
-  def exercises
-    NavLink("/exercises", class: "navlink", active_class: 'active') { "EXERCISES" }
-  end
-
-  def muscle_groups
-    NavLink("/muscle-groups", class: "navlink", active_class: 'active') { "MUSCLE GROUPS" }
-  end
-
-  def rule_set
-    NavLink("/rule-sets", class: "navlink", active_class: 'active') { "RULE SETS" }
   end
 
   def members
