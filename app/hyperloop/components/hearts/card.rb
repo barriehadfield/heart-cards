4be844b319245f94befba0576b12247844d27ceb
Barrie Hadfield <barrie@workshare.com>
2017-08-04 03:34:31 +0100

    mouse in and out

--- app/hyperloop/components/hearts/card.rb
+++ app/hyperloop/components/hearts/card.rb
@@ -2,13 +2,31 @@ class HeartCard < Hyperloop::Component
 
   param :heart
 
+  before_mount do
+    mutate.show_settings false
+  end
+
   render(DIV) do
     Sem.Card(fluid: true) {
       Sem.CardContent {
-        Sem.CardHeader { params.heart.name }
+        Sem.CardHeader { header }
         Sem.CardContent { "and the content here and some more here" }
-        HeartModal(heart: params.heart, mode: :edit)
+      }
+    }
+    .on(:mouse_enter) { mutate.show_settings true }
+    .on(:mouse_leave) { mutate.show_settings false }
+  end
+
+  def header
+    Sem.Grid {
+      Sem.GridRow(columns: 2) {
+        Sem.GridColumn(width: 15) {
+          P { params.heart.name }
+          BR()
+        }
+        Sem.GridColumn(width: 1) { HeartModal(heart: params.heart, mode: :edit) } if state.show_settings
       }
     }
   end
+
 end
