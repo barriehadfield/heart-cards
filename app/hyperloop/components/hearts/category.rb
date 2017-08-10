b858296a51506eb38f4ce943f2b753743c6771b4
Barrie Hadfield <barrie@workshare.com>
2017-08-10 10:45:48 +0100

    spacing

--- app/hyperloop/components/hearts/category.rb
+++ app/hyperloop/components/hearts/category.rb
@@ -29,20 +29,20 @@ class Category < Hyperloop::Component
   end
 
   def category_fields
+    P { "Goals" }
     Sem.Form {
-      P { "Goals" }
       TextInplace(field: "#{params.category}_goals", model: params.heart, label: "Goals",
         placeholder: "What are you trying to achieve?", edit_mode: params.edit_mode)
     }
-
+    BR()
+    P { "Signals" }
     Sem.Form {
-      P { "Signals" }
       TextInplace(field: "#{params.category}_signals", model: params.heart, label: "Signals",
         placeholder: "What signals do you expect to see?", edit_mode: params.edit_mode)
     }
-
+    BR()
+    P { "Metrics" }
     Sem.Form {
-      P { "Metrics" }
       TextInplace(field: "#{params.category}_metrics", model: params.heart, label: "Metrics",
         placeholder: "And how will you measure this?", edit_mode: params.edit_mode)
     }
