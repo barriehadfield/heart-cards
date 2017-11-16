ef472c48a588c7f72b12ddc77bfe746e0c813dad
BarrieHadfield <barrie@tyger.com>
2017-11-16 17:12:35 +0000

    no actioncable

--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -3,7 +3,8 @@
 Hyperloop.configuration do |config|
   config.cancel_import('hyper-router/react-router-source')
   config.compress_system_assets = false
-  config.transport = :action_cable
+  # config.transport = :action_cable
+  config.transport = :none
   config.prerendering = :off
   config.import 'reactrb/auto-import'
 end
