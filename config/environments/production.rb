1535a3ec732a59d48e9091e5c1a7bfad44e34426
BarrieHadfield <barrie@tyger.com>
2017-11-18 11:18:44 +0000

    uglifier options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,14 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+
+  config.assets.js_compressor = Uglifier.new(
+    :beautify => false,
+    :beautify_options => {:indent_level => 0},
+    :comments => :none
+  )
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
