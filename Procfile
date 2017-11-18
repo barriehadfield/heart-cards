6c0f776e785261f1537109adca6aa542ad7e6619
BarrieHadfield <barrie@tyger.com>
2017-11-18 12:21:44 +0000

    :harmony => true

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec puma
+hotloader: opal-hot-reloader -p 25222 -d app/hyperloop
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,11 +30,9 @@ Rails.application.configure do
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # config.assets.js_compressor = Uglifier.new(
-  #   output: {
-  #     comments: :none
-  #   }
-  # )
+  config.assets.js_compressor = Uglifier.new(
+    :harmony => true
+  )
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
