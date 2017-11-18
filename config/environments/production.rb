e0793d3fa77deee76bc9d21d2eea6c22d22711e9
BarrieHadfield <barrie@tyger.com>
2017-11-18 11:37:45 +0000

    no compress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,11 +30,11 @@ Rails.application.configure do
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.assets.js_compressor = Uglifier.new(
-    output: {
-      comments: :none
-    }
-  )
+  # config.assets.js_compressor = Uglifier.new(
+  #   output: {
+  #     comments: :none
+  #   }
+  # )
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
