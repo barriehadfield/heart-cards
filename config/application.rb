b5f6cab34f96a38f423b14656925e1124c42232b
BarrieHadfield <barrie@tyger.com>
2017-11-20 12:47:11 +0000

    application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,14 +15,14 @@ module Hearty
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths   -= %W(#{config.root}/app/hyperloop)
-    config.eager_load_paths -= %W(#{config.root}/app/hyperloop)
-
-    config.eager_load_paths += %W(#{config.root}/app/hyperloop/models)
-    config.autoload_paths += %W(#{config.root}/app/hyperloop/models)
-
-    config.eager_load_paths += %W(#{config.root}/app/hyperloop/operations)
-    config.autoload_paths   += %W(#{config.root}/app/hyperloop/operations)
+    # config.autoload_paths   -= %W(#{config.root}/app/hyperloop)
+    # config.eager_load_paths -= %W(#{config.root}/app/hyperloop)
+    #
+    # config.eager_load_paths += %W(#{config.root}/app/hyperloop/models)
+    # config.autoload_paths += %W(#{config.root}/app/hyperloop/models)
+    #
+    # config.eager_load_paths += %W(#{config.root}/app/hyperloop/operations)
+    # config.autoload_paths   += %W(#{config.root}/app/hyperloop/operations)
 
   end
 end
