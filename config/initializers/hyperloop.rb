c17b30fab16280c40de35dd07ef32283152e5c66
BarrieHadfield <barrie@tyger.com>
2017-11-20 12:51:29 +0000

    hyperloop.rb

--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -1,7 +1,7 @@
 # config/initializers/hyperloop.rb
 # If you are not using ActionCable, see http://ruby-hyperloop.io/docs/models/configuring-transport/
 Hyperloop.configuration do |config|
-  config.cancel_import('hyper-router/react-router-source')
+  # config.cancel_import('hyper-router/react-router-source')
   config.compress_system_assets = false
   config.transport = :pusher
   config.channel_prefix = "Hyperloop"
@@ -13,5 +13,5 @@ Hyperloop.configuration do |config|
     cluster: "mt1"
   }
   config.prerendering = :off
-  config.import 'reactrb/auto-import'
+  # config.import 'reactrb/auto-import'
 end
